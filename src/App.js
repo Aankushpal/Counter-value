9c5c11e79bf71a7cc00ca8558011eb62904e715a
Ankush pal <ankushpal16200@gmail.com>
2024-09-18 17:15:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const[a, b] = useState(5)
+
+  let  AddValue = ()=>{
+    b(a+1)
+  }
+
+  let  DecValue = ()=>{
+    b(a-1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <> 
+      <h1>My React App</h1>
+      <h2>Counter value : {a} </h2>
+      <button onClick={AddValue}>Increase</button>
+      <button onClick={DecValue}>Decrease</button>
+    </>
+  )
 }
 
-export default App;
+export default App
